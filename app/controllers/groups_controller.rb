eed96964bda858bbf76f9a872c2219b51b3b2817
Javier Pacheco <jepach@gmail.com>
2022-04-11 17:40:18 -0500

    Configure Groups Controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,16 +1,35 @@
 class GroupsController < ApplicationController
   def index
+    render json: Group.all
   end
 
   def show
+    render json: @course.groups
   end
 
   def create
+    @group = @course.groups.create(group_params)
   end
 
   def update
+    if @group.update(group_params)
+      render json: @group
+    else
+      render json: {errors: @group.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def delete
+    @group.destroy
+  end
+
+  private
+
+  def set_course
+    @course = Course.find_by(name)
+  end
+
+  def group_params
+    params.permit(:name)
   end
 end
