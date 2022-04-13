c393eb31c6c45430291d39a12d1fca05629021eb
Javier Pacheco <jepach@gmail.com>
2022-04-13 10:16:46 -0500

    Add logic to save data into the Database

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -10,13 +10,53 @@ class UploadsController < ApplicationController
     puts 'Importing Data'
     data = Roo::Spreadsheet.open(params["file"].tempfile) # opens spreadsheet
 
-    headers = data.row(1) # get header row
+    headers = data.row(2) # get header row
+
+    # Creates empty array for excel
+    excel_array = Array.new
+
+    # Iterates spreadsheet
+    data.each_with_index do |row, row_index|
+      if (row_index > 1)
+        # Creates emtpy hash for each row
+        row_hash = Hash.new
+        # Iterates row by header
+        headers.each_with_index do |head, head_index|
+          # Stores each cell in hash
+          row_hash.store("#{head}", row[head_index])
+        end
+        # Stores each has in array
+        excel_array.push(row_hash)
+      end
+    end
+
+    pp excel_array
 
     # Ordena las celdas
     data.each_with_index do |row, index|
-      # user_data = Hash[[headers, row].transpose]
-      if ( index > 1 )
-        pp(row)
+
+      if index > 1 
+        user = User.create(
+              first_name: row[0],
+              last_name: row[1],
+              email: row[3],
+              country: row[4],
+              doc_type: row[5],
+              doc_number: row[6]
+            )
+
+        student = Student.create(
+              first_name: row[7],
+              last_name: row[8],
+              birthdate: row[9],
+              email: row[3],
+              user_id: user.id,
+            )
+
+        enrollment = Enrollment.create(
+              group_id: row[19],
+              student_id: student.id
+        )
       end
     end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,44 +1,9 @@
-puts "Start seeding data"
 
-User.destroy_all
 
-user_data = [{
-  email: 'test@mail.com',
-  first_name: 'Testino',
-  last_name: 'Di Prueba',
-  password: 'letmein',
-  doc_type: 'DNI',
-  doc_number: '01234567',
-  country: 'PE'
-},
-{
-  email: 'test1@mail.com',
-  first_name: 'Jesus',
-  last_name: 'Tuesta',
-  password: 'letmein',
-  doc_type: 'DNI',
-  doc_number: '01234567',
-  country: 'PE'
-},
-{
-  email: 'test2@mail.com',
-  first_name: 'Javier',
-  last_name: 'Pacheco',
-  password: 'letmein',
-  doc_type: 'DNI',
-  doc_number: '01234567',
-  country: 'PE'
-},
-{
-  email: 'test3@mail.com',
-  first_name: 'Alonso',
-  last_name: 'Zevallos',
-  password: 'letmein',
-  doc_type: 'DNI',
-  doc_number: '01234567',
-  country: 'PE'
-}]
+course1 = Course.create(name: "Mini Creador");
+course2 = Course.create(name: "Diseño con Photoshop");
 
-user_data.map {|user| User.create(**user)}
-
-puts 'Finished seeding data'
+group1 = Group.create(course_id: course1.id, name: "jueves a las 4pm");
+group2 = Group.create(course_id: course1.id, name: "martes a las 2pm");
+group3 = Group.create(course_id: course2.id, name: "lunes a las 9am");
+group4 = Group.create(course_id: course2.id, name: "viernes a las 11am");
