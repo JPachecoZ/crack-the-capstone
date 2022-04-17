992498608f7514a69542f675861414896f0781ca
jesustr20 <jesus.tr61@gmail.com>
2022-04-17 17:51:39 -0500

    update code read type file

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -3,106 +3,118 @@ class UploadsController < ApplicationController
   require 'pp'
 
   def upload
+
     puts "Entrando a upload"
 
+    file = params["file"].tempfile
+    type_file = params["file"].original_filename
+    
     p params["file"].tempfile
+    p params["file"].original_filename
+    
+    begin
+    
+      puts 'Importing Data'
+      
+      data = Roo::Spreadsheet.open(file) # opens spreadsheet
 
-    puts 'Importing Data'
-    data = Roo::Spreadsheet.open(params["file"].tempfile) # opens spreadsheet
+      headers = data.row(2) # get header row
 
-    headers = data.row(2) # get header row
+      # # Creates empty array for excel
+      # excel_array = Array.new
 
-    # # Creates empty array for excel
-    # excel_array = Array.new
+      # # Iterates spreadsheet
+      # data.each_with_index do |row, row_index|
+      #   if (row_index > 1)
+      #     # Creates emtpy hash for each row
+      #     row_hash = Hash.new
+      #     # Iterates row by header
+      #     headers.each_with_index do |head, head_index|
+      #       # Stores each cell in hash
+      #       row_hash.store("#{head}", row[head_index])
+      #     end
+      #     # Stores each has in array
+      #     excel_array.push(row_hash)
+      #   end
+      # end
 
-    # # Iterates spreadsheet
-    # data.each_with_index do |row, row_index|
-    #   if (row_index > 1)
-    #     # Creates emtpy hash for each row
-    #     row_hash = Hash.new
-    #     # Iterates row by header
-    #     headers.each_with_index do |head, head_index|
-    #       # Stores each cell in hash
-    #       row_hash.store("#{head}", row[head_index])
-    #     end
-    #     # Stores each has in array
-    #     excel_array.push(row_hash)
-    #   end
-    # end
+      # pp excel_array
 
-    # pp excel_array
-
-    data_to_return = {
-      registers_created: 0,
-      registers_with_errors: 0,
-      total_data_analized: 0,
-      data_to_inspect: []
-    }
+      data_to_return = {
+        registers_created: 0,
+        registers_with_errors: 0,
+        total_data_analized: 0,
+        data_to_inspect: []
+      }
     
     # Ordena las celdas
-    data.each_with_index do |row, index|
+      data.each_with_index do |row, index|
 
-      if index > 1
+        if index > 1
 
-        if User.find_by(email: row[3]) == nil
+          if User.find_by(email: row[3]) == nil
 
-          user = User.create(
-              first_name: row[0],
-              last_name: row[1],
-              email: row[3],
-              password: "capstone2022",
-              country: row[4],
-              doc_type: row[5],
-              doc_number: row[6]
-            )
+            user = User.create(
+                first_name: row[0],
+                last_name: row[1],
+                email: row[3],
+                password: "capstone2022",
+                country: row[4],
+                doc_type: row[5],
+                doc_number: row[6]
+              )
 
-        p user.errors.messages
+          p user.errors.messages
 
-        else user = User.find_by(email: row[3])
+          else user = User.find_by(email: row[3])
+          
+          end
+
+          if Student.find_by(first_name: row[10]) == nil || Student.find_by(first_name: row[10]) != nil && Student.find_by(first_name: row[10]).user_id != user.id
+
+          student = Student.create(
+                first_name: row[10],
+                last_name: row[11],
+                birthdate: row[13],
+                email: row[3],
+                user_id: user.id,
+              )
+            
+          p student.errors.messages
+
+          else student = Student.find_by(first_name: row[10])
+
+          end
+
+          enrollment = Enrollment.create(
+                group_id: row[23],
+                student_id: student.id
+          )
+
+          p enrollment.errors.messages
         
-        end
+          data_to_return[:total_data_analized] += 1
 
-        if Student.find_by(first_name: row[10]) == nil || Student.find_by(first_name: row[10]) != nil && Student.find_by(first_name: row[10]).user_id != user.id
+          if user.errors.size != 0 || student.errors.size != 0 || enrollment.errors.size != 0
+            # Defines where to start printing the errors.
+            i = row.length + 1
 
-        student = Student.create(
-              first_name: row[10],
-              last_name: row[11],
-              birthdate: row[13],
-              email: row[3],
-              user_id: user.id,
-            )
-        
-        p student.errors.messages
-
-        else student = Student.find_by(first_name: row[10])
-
-        end
-
-        enrollment = Enrollment.create(
-              group_id: row[23],
-              student_id: student.id
-        )
-
-        p enrollment.errors.messages
-      
-        data_to_return[:total_data_analized] += 1
-
-        if user.errors.size != 0 || student.errors.size != 0 || enrollment.errors.size != 0
-          # Defines where to start printing the errors.
-          i = row.length + 1
-
-          row[i] = user.errors
-          row[i+1] = student.errors
-          row[i+2] = enrollment.errors
-          data_to_return[:data_to_inspect] << row
-          data_to_return[:registers_with_errors] += 1
-        else
-          data_to_return[:registers_created] += 1
+            row[i] = user.errors
+            row[i+1] = student.errors
+            row[i+2] = enrollment.errors
+            data_to_return[:data_to_inspect] << row
+            data_to_return[:registers_with_errors] += 1
+          else
+            data_to_return[:registers_created] += 1
+          end
         end
       end
+
+      render json: {data: data_to_return}, status: :ok
+      puts "Rake end!"
+    rescue
+      render json: {data: data_to_return, meesage: 'File type not Allowed'}, status: :unprocessable_entity
     end
 
-    render json: {data: data_to_return}, status: :ok
-    puts "Rake end!"
   end
 end
